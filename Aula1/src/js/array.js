cc6cf4802aad913b514c06aba5bd62c60dc74448
yDenBarbosa <denis9195@hotmail.com>
2025-10-18 15:13:22 -0300

    feat:arrays

--- Aula1/src/js/array.js
+++ Aula1/src/js/array.js
@@ -1,4 +1,12 @@
+
 let frutas = ["Maçã", "Banana", "Uva", "Melancia", "Laranja"]
 console.log(frutas)
 console.log(frutas[2]) //Maçã
-console.table(frutas)
+console.table(frutas) //.table = tabela
+console.log(frutas[3],frutas[0])
+
+let idades = [13, 32, 53, 54, 34]
+console.log(idades[2])
+console.table(idades)
+
+let mercado = [ "Arroz", 20, "Melancia", true]
