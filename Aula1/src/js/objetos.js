bcb844ebecd1e776cdd5c3c131dd9be6e8ff0d86
yDenBarbosa <denis9195@hotmail.com>
2025-10-18 16:22:08 -0300

    feat:objetos e atividades

--- Aula1/src/js/objetos.js
+++ Aula1/src/js/objetos.js
@@ -9,3 +9,11 @@ console.log(alunos.nome)
 console.log(alunos.idade)
 console.log(alunos.nome, alunos.idade)
 console.log(`O aluno ${alunos.nome} tem ${alunos.idade} anos`)
+
+let livro = {
+    titulo: "Fogo e gelo",
+    autor: "Toninho",
+    paginas: 432
+}
+
+console.log(`O livro ${livro.titulo} do autor ${livro.autor} tem ${livro.paginas} páginas`)
