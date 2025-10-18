1fd55d638f5a69d7f66ec60f5b0725cd1a7cd81a
yDenBarbosa <denis9195@hotmail.com>
2025-10-18 14:46:47 -0300

    feat:variaveis e dados

---
+++ Aula1/src/js/tiposDeDados.js
@@ -0,0 +1,21 @@
+// -------------------- STRING -----------------------
+var nome = "Denis"
+let nome1 = "Barbosa"
+const nome2 = "Silva"
+
+console.log(nome)
+console.log(nome1)
+console.log(nome2)
+
+// -------------------- NUMBER -----------------------
+let idade = 22
+
+// -------------------- BOOLEAN -----------------------
+let maiorDeIdade = true
+let menorDeIdade = false
+
+// -------------------- NULL -----------------------
+let desconto = null
+
+// -------------------- UNDEFINED -----------------------
+let teste
---
+++ Aula1/src/js/tiposDeVariaveis.js
@@ -0,0 +1,25 @@
+// console.log("Olá!")
+// console.log("Tudo bem?")
+// console.log("Outra mensagem")
+// console.log(1+1)
+
+// tipo nome = valor
+
+// var: variavel mutavel, permite a redeclaração de variavel ex:
+var nome = "Denis"
+console.log(nome)
+nome = "Bolacha"
+console.log(nome)
+
+var idade = 29
+idade = 92
+console.log(idade)
+
+// let: variavel mutavel, não permite a redeclaração de variavel ex:
+let idade = 43
+let idade = 34
+
+// const: variavel não mutavel
+const pi = 3.14
+// pi = 10
+console.log(pi)
