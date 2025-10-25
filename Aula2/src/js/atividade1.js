35a9ae6b2e725bc5d2b4f61c8fac0da24e2aa673
yDenBarbosa <denis9195@hotmail.com>
2025-10-25 09:34:04 -0300

    feat:atividades

---
+++ Aula2/src/js/atividade1.js
@@ -0,0 +1,3 @@
+let idade = 10
+
+console.log(`Faltam ${100 - idade} anos para completar 100 anos`)
---
+++ Aula2/src/js/atividade2.js
@@ -0,0 +1,2 @@
+let num = 3
+console.log(`O dobro de ${num} é ${num * 2} e a metade de ${num} é ${num / 2}`)
