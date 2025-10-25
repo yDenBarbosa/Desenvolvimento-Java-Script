466096923b20dba5698774d49e1d7388a370df45
yDenBarbosa <denis9195@hotmail.com>
2025-10-25 10:32:47 -0300

    feat:operadoresComparacao

--- Aula2/src/js/operadoresComparacao.js
+++ Aula2/src/js/operadoresComparacao.js
@@ -1,3 +1,20 @@
 let x = 3 
-let y = 7
+let y = 8
+
+console.log(x == y) // igualdade (compara somente o valor)
+console.log(x === y) // igualdade (valor e o tipo)
+console.log(x != y) // diferente (se um numero é diferente do outro)
+console.log(x !== y) // diferente (valor e tipo)
+
+console.log(x > y) // Maior que
+console.log(x >= y) // Maior ou igual
+console.log(x < y) // Menor
+console.log(x <= y) // Menor ou igual
+
+let idade1 = 34
+let idade2 = 43
+console.log(idade1 > idade2)
+console.log(idade1 == idade2)
+console.log(idade1 != idade2)
+
 
