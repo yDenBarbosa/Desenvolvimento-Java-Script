bad926b600abdd5fe2257d43e7ec1af6ba06b254
yDenBarbosa <denis9195@hotmail.com>
2025-10-25 09:06:48 -0300

    feat:operadoresAritmeticos

---
+++ Aula2/src/js/operadoresAritmeticos.js
@@ -0,0 +1,11 @@
+// -------- SOMA --------
+
+let a = 10
+let b = 3
+
+console.log(a + b) // soma +
+console.log(a - b) // subtracao -
+console.log(a * b) // multiplicacao -
+console.log(a / b) // divisao /
+console.log(a % b) // resto da divisao %
+console.log(a ** b) // potencia **
