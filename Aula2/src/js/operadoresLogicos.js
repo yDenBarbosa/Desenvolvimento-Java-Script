108df9cdb84bdbb32551bd1712f46e5961f0149e
yDenBarbosa <denis9195@hotmail.com>
2025-10-25 11:22:25 -0300

    feat:operadoresLogicos

---
+++ Aula2/src/js/operadoresLogicos.js
@@ -0,0 +1,30 @@
+// ------------------ && (AND) ------------------
+
+const pipoca = true
+const chocolate = false
+
+console.log(pipoca == true && chocolate == true)
+
+// ------------------ || (OR) --------------------
+
+const lapis = true
+const caneta = false
+
+console.log(lapis == true || caneta == true)
+
+// ----------------- NOT (!) --------------------
+
+const logado = false
+
+console.log(`Valor original: ${logado}`)
+console.log(`Valor original: ${!logado}`)
+
+let valorCompra = 34
+let clienteFrequente = true
+console.log(`Promoção aplicada: ${valorCompra > 100 && clienteFrequente == true}\nvalor da compra: ${valorCompra}`)
+
+let temConvite = false
+let estaNaLista = true
+let entrada = temConvite == true || estaNaLista == true
+
+console.log(`Entrada permitida: ${entrada}`)
