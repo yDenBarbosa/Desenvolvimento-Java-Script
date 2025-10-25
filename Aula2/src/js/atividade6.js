18f7baa6c198acce54c6f3be5ec45bc6107d0ced
yDenBarbosa <denis9195@hotmail.com>
2025-10-25 16:36:54 -0300

    feat:atividade6

---
+++ Aula2/src/js/atividade6.js
@@ -0,0 +1,15 @@
+let mes = 5
+
+switch (mes) {
+    case 1:
+        console.log("Janeiro")
+        break
+    case 2:
+        console.log("Fevereiro")
+        break
+    case 3:
+        console.log("Março")
+        break
+    default:
+        console.log("Mês inválido")
+}
---
+++ Aula2/src/js/estruturaCondicionalSwitc.js
@@ -0,0 +1,24 @@
+let dia = 5
+
+switch(dia){
+    case 1:
+        console.log("Domingo")
+        break
+    case 2:
+        console.log("Segunda")
+        break
+    case 3:
+        console.log("Terça")
+        break
+    case 4:
+        console.log("Quarta")
+        break
+    case 5:
+        console.log("Quinta")
+        break
+    case 6:
+        console.log("Sexta")
+        break
+    default:
+        console.log("Dia da semana inválido")
+}
