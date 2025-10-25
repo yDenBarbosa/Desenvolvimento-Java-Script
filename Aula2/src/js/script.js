ea8fec6a066e8b0e1a9a1e977fa80ec11fe44452
yDenBarbosa <denis9195@hotmail.com>
2025-10-25 08:42:35 -0300

    feat:revisao

--- Aula2/src/js/script.js
+++ Aula2/src/js/script.js
@@ -3,4 +3,7 @@ let idade = 20
 const maiorDeIdade = false
 
 let frutas = ["Maçã", "Uva", "Morango"]
+let nomes = ["Denis", "Vitoria", "Xter", "Neymar"]
+
 console.log(frutas)
+console.log(nomes[1])
