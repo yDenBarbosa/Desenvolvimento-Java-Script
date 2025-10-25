b8be5d060af1ea4abcedd7ecd994742b908c05a8
yDenBarbosa <denis9195@hotmail.com>
2025-10-25 14:48:25 -0300

    feat:if, else if e else

--- Aula2/src/js/estruturaCondicionail.js
+++ Aula2/src/js/estruturaCondicionail.js
@@ -26,3 +26,17 @@ if (temperatura < 15) {
 } else {
     console.log("Está quente!")
 }
+
+let nota = 7
+// nota maior que 7 -> Aprovado
+// nota entre que 5, 6 e 7 -> Recuperação
+// nota menor que 5 -> Reprovado
+
+if(nota > 7){
+    console.log("Aprovado")
+} else if(nota >= 5){
+    console.log("Recuperação")
+} else{
+    console.log("Reprovado")
+}
+
