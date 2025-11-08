840ae0ddbcae889d3e2e0e42627a945fdeba82f7
yDenBarbosa <denis9195@hotmail.com>
2025-11-08 09:16:42 -0300

    feat:arrays e objetos

---
+++ Aula4/src/js/array.js
@@ -0,0 +1,13 @@
+let numeros = [2, 4, 6, 8]
+// console.log(numeros)
+
+let frutas = ["abacaxi", "melancia", "uva"]
+
+// frutas.unshift("banana") //ADICIONA NO COMEÇO
+// frutas.push("morango") //ADICIONA NO FIM
+// frutas.splice(2, 0, "maçã") //ADICIONA POR POSIÇÃO/INDICE
+
+frutas.shift() //REMOVE O PRIMEIRO
+frutas.pop() //REMOVE O ULTIMO
+
+console.log(frutas)
---
+++ Aula4/src/js/objeto.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    nome: "Denis",
+    idade: 29,
+    email: "email@uat.com"
+}
+
+console.log(pessoa)
+console.log(pessoa.idade) //ponto para uma chave especifica
+console.log(pessoa.email,pessoa.nome) // virgula para adicionar mais uma chave
