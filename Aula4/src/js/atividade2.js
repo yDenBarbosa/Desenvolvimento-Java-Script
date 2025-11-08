206fdc03b72ba0c191592130bddc0f42a69995b9
yDenBarbosa <denis9195@hotmail.com>
2025-11-08 16:18:45 -0300

    feat:atividade

--- Aula4/src/js/atividade2.js
+++ Aula4/src/js/atividade2.js
@@ -1,8 +1,14 @@
 let btn = document.getElementById('btn')
+let btnz = document.getElementById('btnz')
 let titulo = document.getElementById('titulo')
 let contador = 0
 
 btn.addEventListener('click', function(){
     contador += 1
+    titulo.innerText = `Você clicou ${contador} vezes`
+})
+
+btnz.addEventListener('click', function(){
+    contador = 0
     titulo.innerText = `Você clicou ${contador} vezes`
 })
---
+++ Aula4/src/js/atividade3.js
@@ -0,0 +1,6 @@
+let parag = document.getElementById('paragrafo')
+let btn = document.getElementById('btn')
+
+btn.addEventListener("click", function(){
+    parag.innerText = "Texto alterado com sucesso!"
+})
